f214ea56b6f685a0256d25c9c97cb58f06a6c948
Pevneva <millo4ka@list.ru>
2016-12-15 17:02:52 +0300

    Fixed task10_01 test.

--- src/test/java/comqa/task10_01.java
+++ src/test/java/comqa/task10_01.java
@@ -1,6 +1,7 @@
 package comqa;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.logging.LogEntry;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class task10_01 extends TestBase {
             loginToAdminApp();
             listResults.clear();
             checkAllProductsFromCatalogCategory();
+            tempVoid();
             assertTrue(isAllresultsOk(listResults));
         } catch (NumberFormatException e) {
             System.out.println("Some pages contains browser errors!");
@@ -45,21 +47,40 @@ public class task10_01 extends TestBase {
             boolean res = isBrowserNotErrors();
             listResults.add(res);
             if (!res) {
-                System.out.println("Browser errors were found!");
+                System.out.println("Browser errors were found on the following page:");
                 System.out.println(driver.getCurrentUrl());
             }
 
             driver.get(urlCatalogCategory);
+
         }
     }
 
-    public boolean isBrowserNotErrors() {
-        if (driver.manage().logs().get("browser").filter(Level.ALL).size() > 0) {
-            driver.manage().logs().get("browser").forEach(l -> {
-                System.out.println("Errors were found on the page:" + l);
-            });
+    public void tempVoid() {
+
+        driver.get("http://localhost:70/litecart/admin/?app=orders&doc=orders");
+        driver.findElement(By.xpath("//a[@class='button']")).click();
+
+        boolean res = isBrowserNotErrors();
+        listResults.add(res);
+        if (!res) {
+            System.out.println("Browser errors were found on the following page:");
+            System.out.println(driver.getCurrentUrl());
         }
-        return driver.manage().logs().get("browser").filter(Level.ALL).size() > 0;
+
+    }
+
+    public boolean isBrowserNotErrors() {
+
+        List<LogEntry> listLogs = driver.manage().logs().get("browser").getAll();
+        System.out.println("SIZE:" + listLogs.size());
+        if (listLogs.size() > 0) {
+            for (int i = 0; i < listLogs.size(); i++) {
+                System.out.println("MESSAGE:" + listLogs.get(i));
+            }
+        }
+
+        return listLogs.size() == 0;
     }
 
 
